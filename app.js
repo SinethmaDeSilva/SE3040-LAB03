b55a550c67acb440e659186a333cd4bdd15009d6
Sinethma de Silva <sinethmakavithaki@gmail.com>
2026-02-23 21:18:33 +0530

    Promises

--- app.js
+++ app.js
@@ -1,37 +1,17 @@
-const myModule = require('./my-module.js');
-console.log(myModule.myFunction());
+const condition = true;
 
-const fs = require('fs');
-
-fs.writeFile('file.txt', 'Hello World!', function (err) {
-  if (err) throw err;
-  console.log('File saved!');
+const myPromise = new Promise((resolve, reject) => {
+  if (condition) {
+    resolve('Success!');
+  } else {
+    reject('Failure!');
+  }
 });
 
-const http = require('http');
-
-http
-  .createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.write('Hello World!');
-    res.end();
+myPromise
+  .then((result) => {
+    console.log(result);
   })
-  .listen(8080);
-
-const https = require('https');
-
-https
-  .get('https://jsonplaceholder.typicode.com/posts/1', (resp) => {
-    let data = '';
-
-    resp.on('data', (chunk) => {
-      data += chunk;
-    });
-
-    resp.on('end', () => {
-      console.log(JSON.parse(data));
-    });
-  })
-  .on('error', (err) => {
-    console.log('Error: ' + err.message);
+  .catch((error) => {
+    console.log(error);
   });
